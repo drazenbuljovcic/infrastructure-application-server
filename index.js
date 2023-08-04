37b2359fdbe43da0ff486f1ca18f1c138297d4a6
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2023-08-04 09:56:40 +0200

    instrumentation

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
+require("dotenv").config();
+
+// you have to use the tracer before importing express!!
+require("./tracer")();
+
+const { context } = require("@opentelemetry/api");
+
 const express = require("express");
 var morgan = require("morgan");
 
-require("dotenv").config();
+console.log({ OTEL_API_HOST: process.env.OTEL_API_HOST });
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
 const permissions = {
   consumers: [{ identifier: "My-Living-Room", permissions: "ALL" }],
 };
 
+app.get("/", async (req, res) => {
+  const activeSpanContext = context.active();
+  // const activeSpan = trace.getSpan(activeSpanContext);
+
+  // if (activeSpan) {
+  //   console.log("Active Span Name:", activeSpan.name);
+  //   console.log("Active Trace ID:", activeSpan.spanContext().traceId);
+  //   console.log("Active Span ID:", activeSpan.spanContext().spanId);
+  // }
+
+  req.activeSpanContext = activeSpanContext;
+  console.log("REDIRECTING", { activeSpanContext });
+  return res.redirect(302, "/distributed-information-node");
+});
+
 app.get("/distributed-information-node", async (req, res) => {
   const { sleep } = req.query;
 
-  if (sleep) {
-    await new Promise((resolve) => setTimeout(resolve, sleep || 2000));
-  }
+  // const activeSpanContext = req.activeSpanContext;
+  // const activeSpan = trace.getSpan(activeSpanContext);
 
+  // console.log("Active Span Name:", activeSpan.name);
+  // console.log("Active Trace ID:", activeSpan.spanContext().traceId);
+  // console.log("Active Span ID:", activeSpan.spanContext().spanId);
+
+  console.log({ permissions });
+  if (sleep) {
+    await new Promise((resolve) => setTimeout(resolve, sleep));
+  }
   return res.json(permissions);
 });
 
---
+++ tracer.js
@@ -0,0 +1,58 @@
+const { trace, context } = require("@opentelemetry/api");
+const { Resource } = require("@opentelemetry/resources");
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
+
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+const {
+  ExpressInstrumentation,
+} = require("opentelemetry-instrumentation-express");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
+module.exports = () => {
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
+    }),
+  });
+
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(
+      new ZipkinExporter({
+        url: `${process.env.OTEL_API_HOST}/api/v2/spans`,
+        serviceName: process.env.OTEL_SERVICE_NAME,
+      })
+    )
+  );
+  provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+  // provider.addSpanProcessor(
+  //   new BatchSpanProcessor(
+  //     new OTLPTraceExporter({
+  //       url: process.env.OTEL_API_HOST,
+  //     })
+  //   )
+  // );
+
+  provider.register();
+
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation({ includeHttpAttributes: true }),
+    ],
+    tracerProvider: provider,
+  });
+
+  const tracer = trace.getTracer(process.env.OTEL_SERVICE_NAME);
+  tracer.startSpan("ROOT", {}, context.ROOT_CONTEXT);
+
+  return { tracer };
+};
