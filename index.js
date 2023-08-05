3746e2f1929b0d443b7c18348ca885116a04306b
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2023-08-05 18:32:02 +0200

    minor

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.get("/", async (req, res) => {
 
 app.get("/distributed-information-node", async (req, res) => {
   const { sleep } = req.query;
+  console.log({ headers: req.headers });
 
   // const activeSpanContext = req.activeSpanContext;
   // const activeSpan = trace.getSpan(activeSpanContext);
--- tracer.js
+++ tracer.js
@@ -8,9 +8,9 @@ const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
 const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
-const {
-  OTLPTraceExporter,
-} = require("@opentelemetry/exporter-trace-otlp-http");
+// const {
+//   OTLPTraceExporter,
+// } = require("@opentelemetry/exporter-trace-otlp-http");
 const {
   ExpressInstrumentation,
 } = require("opentelemetry-instrumentation-express");
@@ -32,7 +32,7 @@ module.exports = () => {
       })
     )
   );
-  provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+  // provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
   // provider.addSpanProcessor(
   //   new BatchSpanProcessor(
   //     new OTLPTraceExporter({
